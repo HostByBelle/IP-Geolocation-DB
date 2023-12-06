8d29d715dde4ce99b5f9d2951bff8e36e9b769fa
Belle Aerni <belleaerni@gmail.com>
2023-12-06 01:13:08 -0800

    This *might* do what I want

--- tests/pingdom-probe-test.py
+++ tests/pingdom-probe-test.py
@@ -5,15 +5,34 @@ import sys
 def parse_xml(xml_file, geoip_db, ip_type):
     tree = ET.parse(xml_file)
     root = tree.getroot()
+    
+    total = 0
+    totalWrong = 0
 
     for item in root.findall('.//item'):
+        # First we need to extract the needed data from Pingdom's feed
         ip_address = item.find(f'pingdom:{ip_type}', {'pingdom': 'http://www.pingdom.com/ns/PingdomRSSNamespace'}).text
+        country_element = item.find('pingdom:country', {'pingdom': 'http://www.pingdom.com/ns/PingdomRSSNamespace'})
+        country_code = country_element.attrib.get('code', '')
+        #city = item.find('pingdom:country', {'pingdom': 'http://www.pingdom.com/ns/PingdomRSSNamespace'}).text
 
-        # Perform IP geolocation with MaxMind
+
+        # Then pull the associated data from the database
         location_data = get_location_data(geoip_db, ip_address)
-        print(f"Country: {location_data['country']}")
-        print(f"City: {location_data['city']}")
-        print(f"State: {location_data['state']}")
+
+        # And finally, valdiate that they are correct
+        if(country_code and location_data['country']):
+            total+=1
+            if(country_code != location_data['country']):
+                # TODO: Do I want to record the addresses that were wrong?
+                #print(f"{ip_address} Should be in {country_code}, but was instead responded as {location_data['country']}")
+                totalWrong+=1
+
+    if(total):
+        accuracy = 100 - round(totalWrong / total * 100, 2)
+        print(f"- Out of {total} Pingdom IP addreses tested, this database failed {totalWrong} for a total accuracy of {accuracy}%")
+    else:
+        print(f"- This database does not contain the neded info to perform the Pingdom test")
 
 def get_location_data(geoip_db, ip_address):
     with maxminddb.open_database(geoip_db) as reader:
@@ -22,10 +41,9 @@ def get_location_data(geoip_db, ip_address):
             return {
                 'country': location_data['country']['iso_code'],
                 'city': location_data.get('city', {}).get('names', {}).get('en', ''),
-                'state': location_data.get('subdivisions', [{}])[0].get('names', {}).get('en', ''),
             }
         except Exception as e:
-            print(f"Error retrieving location data for IP {ip_address}: {e}")
+            print(f"- Error retrieving location data for IP {ip_address}: {e}")
             return None
 
 def main():
