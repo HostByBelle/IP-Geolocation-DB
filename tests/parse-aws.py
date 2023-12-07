4650898a7d317b918499e2fbe4ab71d469afd20a
Belle Aerni <belleaerni@gmail.com>
2023-12-07 00:19:55 -0800

    First attempted run with AWS IPs

--- tests/parse-aws.py
+++ tests/parse-aws.py
@@ -1,7 +1,100 @@
 import argparse
 import json
 from progressbar import progressbar
-from pprint import pprint
+
+# Manually gathered from https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html
+region_info = {
+    'us-east-2': {
+        'country_code': 'US',
+    },
+    'us-east-2': {
+        'country_code': 'US',
+    },
+    'us-west-1': {
+        'country_code': 'US',
+    },
+    'us-west-2': {
+        'country_code': 'US',
+    },
+    'af-south-1': {
+        'country_code': 'ZA',
+    },
+    'ap-east-1': {
+        'country_code': 'HK',
+    },
+    'ap-south-2': {
+        'country_code': 'IN',
+    },
+    'ap-southeast-3': {
+        'country_code': 'ID',
+    },
+    'ap-southeast-4': {
+        'country_code': 'AU',
+    },
+    'ap-south-1': {
+        'country_code': 'IN',
+    },
+    'ap-northeast-3': {
+        'country_code': 'JP',
+    },
+    'ap-northeast-2': {
+        'country_code': 'KR',
+    },
+    'ap-southeast-1': {
+        'country_code': 'SG',
+    },
+    'ap-southeast-2': {
+        'country_code': 'AU',
+    },
+    'ap-northeast-1': {
+        'country_code': 'JP',
+    },
+    'ca-central-1': {
+        'country_code': 'CA',
+    },
+    'eu-central-1': {
+        'country_code': 'DE',
+    },
+    'eu-west-1': {
+        'country_code': 'IE',
+    },
+    'eu-west-2': {
+        'country_code': 'GB',
+    },
+    'eu-south-1': {
+        'country_code': 'IT',
+    },
+    'eu-west-3': {
+        'country_code': 'FR',
+    },
+    'eu-south-2': {
+        'country_code': 'ES',
+    },
+    'eu-north-1': {
+        'country_code': 'SE',
+    },
+    'eu-central-2': {
+        'country_code': 'CH',
+    },
+    'il-central-1': {
+        'country_code': 'IL',
+    },
+    'me-south-1': {
+        'country_code': 'BH',
+    },
+    'me-central-1': {
+        'country_code': 'AE',
+    },
+    'sa-east-1': {
+        'country_code': 'BR',
+    },
+    'us-gov-east-1': {
+        'country_code': 'US',
+    },
+    'us-gov-west-1': {
+        'country_code': 'US',
+    },
+}
 
 def parse(updown_data, json_file, ipver):
     data_list = []
@@ -18,16 +111,17 @@ def parse(updown_data, json_file, ipver):
         for prefix in progressbar(aws_ips['prefixes']):
             prefix_key = ipver + '_prefix'
             if(prefix[prefix_key]):
-                data_list.append({
-                    'ip_range': prefix[prefix_key],
-                    'country_code': 'US',
-                    'city': 'Test'
-                })
-        #print(f"Completed prefix: {prefix[prefix_key]}")
+                if prefix['region'] in region_info:
+                    data_list.append({
+                        'ip_range': prefix[prefix_key],
+                        **region_info[prefix['region']]
+                    })
+                else:
+                    print(f"{prefix['region']} is not yet mapped")
 
         # Write the updated data back to the JSON file
-        #with open(json_file, 'w', encoding='utf-8') as json_file:
-            #json.dump(data_list, json_file, indent=4, ensure_ascii=False)
+        with open(json_file, 'w', encoding='utf-8') as json_file:
+            json.dump(data_list, json_file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
