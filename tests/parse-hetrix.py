aa9fd5ef58526ccd194b1eadd9b345d1483a7428
Belle Aerni <belleaerni@gmail.com>
2023-12-06 18:58:33 -0800

    Mini refactor of code

--- tests/parse-hetrix.py
+++ tests/parse-hetrix.py
@@ -21,87 +21,68 @@ def parse(file_path, json_file):
 
     with open(file_path, 'r') as file:
         for line in file:
-            parts = line.split()
-            if len(parts) >= 2:
-                # Extract the hostname and IP address
-                hostname = parts[0]
-                ip_address = parts[1]
+            match = re.match(r'^(\S+)\s+(\S+)', line)
+            if match:
+                hostname = match.group(1)
+                ip_address = match.group(2)
                 wk = extract_wk(hostname)
 
                 if hostname and ip_address and wk:
-                    match wk:
-                        case 'wk1':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'US',
-                                'city': 'New York'
-                            })
-                        case 'wk2':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'US',
-                                'city': 'San Francisco'
-                            })
-                        case 'wk3':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'NL',
-                                'city': 'Amsterdam'
-                            })
-                        case 'wk4':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'GB',
-                                'city': 'London'
-                            })
-                        case 'wk5':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'DE',
-                                'city': 'Frankfurt'
-                            })
-                        case 'wk6':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'SG',
-                                'city': 'Singapore'
-                            })
-                        case 'wk7':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'US',
-                                'city': 'Dallas'
-                            })
-                        case 'wk8':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'AUS',
-                                'city': 'Sydney'
-                            })
-                        case 'wk9':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'BR',
-                                'city': 'Sao Paulo'
-                            })
-                        case 'wk10':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'JP',
-                                'city': 'Tokyo'
-                            })
-                        case 'wk11':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'IN',
-                                'city': 'Mumbai'
-                            })
-                        case 'wk12':
-                            data_list.append({
-                                'ip_address': ip_address,
-                                'country_code': 'PL',
-                                'city': 'Warsaw'
-                            })
+                    wk_mapping = {
+                        'wk1': {
+                            'country_code': 'US',
+                            'city': 'New York'
+                        },
+                        'wk2': {
+                            'country_code': 'US',
+                            'city': 'San Francisco'
+                        },
+                        'wk3': {
+                            'country_code': 'NL',
+                            'city': 'Amsterdam'
+                        },
+                        'wk4': {
+                            'country_code': 'GB',
+                            'city': 'London'
+                        },
+                        'wk5': {
+                            'country_code': 'DE',
+                            'city': 'Frankfurt'
+                        },
+                        'wk6': {
+                            'country_code': 'SG',
+                            'city': 'Singapore'
+                        },
+                        'wk7': {
+                            'country_code': 'US',
+                            'city': 'Dallas'
+                        },
+                        'wk8': {
+                            'country_code': 'AUS',
+                            'city': 'Sydney'
+                        },
+                        'wk9': {
+                            'country_code': 'BR',
+                            'city': 'Sao Paulo'
+                        },
+                        'wk10': {
+                            'country_code': 'JP',
+                            'city': 'Tokyo'
+                        },
+                        'wk11': {
+                            'country_code': 'IN',
+                            'city': 'Mumbai'
+                        },
+                        'wk12': {
+                            'country_code': 'PL',
+                            'city': 'Warsaw'
+                        }
+                    }
+                    if wk in wk_mapping:
+                        data_list.append({
+                            'ip_address': ip_address,
+                            **wk_mapping[wk]
+                        })
 
         # Write the updated data back to the JSON file
         with open(json_file, 'w', encoding='utf-8') as json_file:
